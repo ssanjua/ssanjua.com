0f45763fdb42723684408faa1806859d83629ced
ssanjua <ppaupallares@gmail.com>
2024-06-17 16:27:11 +0200

    add i18n

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,6 @@
-import { ui, defaultLang } from './ui';
+import { ui, defaultLang } from './ui'
+
+export const showDefaultLang = false
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
@@ -9,5 +11,11 @@ export function getLangFromUrl(url: URL) {
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
+  }
+}
+
+export function useTranslatedPath(lang: keyof typeof ui) {
+  return function translatePath(path: string, l: string = lang) {
+    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`
   }
 }
