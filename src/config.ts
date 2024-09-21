fa725c2313a66f5d5679ec1a97504870da5d752d
ssanjua <ppaupallares@gmail.com>
2024-09-21 19:05:57 +0200

    real last og twitter card try

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 export const SITE = {
   website: "https://ssanjua.vercel.app/",
   ogImage: "oG-ssanjua.webp",
-  author: "Pau Pallares ssanjua"
+  author: "Pau Pallares ssanjua",
+  title: "ssanjua - full stack web developer - portfolio ",
+  description: "full stack web developer based in spain, open for work"
 }
