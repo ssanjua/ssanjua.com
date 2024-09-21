f431e6625a26ebb819acb949afc5e0936a96dc8c
ssanjua <ppaupallares@gmail.com>
2024-09-21 18:57:02 +0200

    last chance for twitter card

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ssanjua.vercel.app',
   integrations: 
   [
     tailwind(), 
